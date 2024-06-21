970018dd21e8be921b3fe25cf59901e8c3e25373
YaseenAT <yaseen507at@gmail.com>
2024-06-21 22:43:49 +0530

    pages loging

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,42 +7,47 @@ import {
   useNavigate,
   useLocation,
 } from "react-router-dom";
+
 export default function Login() {
+  const nav = useNavigate();
 
-
-// console.log(arr.)
+  // console.log(arr.)
   return (
-    <div className={css.homeBody}   >
+    <div className={css.homeBody}>
       <div className={css.home}>
-          <div className={css.loginpage}>
-            <div className={css.headingText}>
+        <div className={css.loginpage}>
+          <div className={css.headingText}>
             <label htmlFor="logIn">Sign In</label>
+          </div>
+          <div className={css.miniBody}>
+            <div className={css.head1}>
+              {/* <label>User Name</label> */}
+              <input type="text" placeholder="Enter User Name" />
             </div>
-            <div className={css.miniBody}>
-              <div className={css.head1}>
-                {/* <label>User Name</label> */}
-                <input type="text" placeholder="Enter User Name" />
-              </div>
-              <div className={css.head1}>
-                {/* <label>Password</label> */}
-                <input type="password" placeholder="Enter Password" />
-              </div>
-              <div> create account?  <span>
-                <a rel="stylesheet" href="/">dfdfd</a> 
+            <div className={css.head1}>
+              {/* <label>Password</label> */}
+              <input type="password" placeholder="Enter Password" />
+            </div>
+            <div>
+              {" "}
+              create account?{" "}
+              <span>
+                <a rel="stylesheet" href="/">
+                  dfdfd
+                </a>
               </span>
-              </div>
-              <button 
-              onClick={() => {
-               alert("Log In ");
-              }}
-                >
-              
-                Log In
-              </button>
             </div>
+            <button
+              onClick={() => {
+                alert("Log In ");
+              }}
+            >
+              Log In
+            </button>
+            <button onClick={nav(-1)}>Back</button>
           </div>
         </div>
+      </div>
     </div>
   );
 }
-
