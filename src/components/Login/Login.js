fca14abf661bf22ad81c0ef8b707516e22ea0caa
unknown <yaseen507at@gmail.com>
2024-12-16 22:24:41 +0530

    login page Design and Development

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,7 +7,7 @@ import {
   useNavigate,
   useLocation,
 } from "react-router-dom";
-
+import snaks from "../../assets/snaks_bg12.png"
 export default function Login() {
   const nav = useNavigate();
 
@@ -15,38 +15,55 @@ export default function Login() {
   return (
     <div className={css.homeBody}>
       <div className={css.home}>
-        <div className={css.loginpage}>
-          <div className={css.headingText}>
-            <label htmlFor="logIn">Sign In</label>
+        <div className={css.upperBox} >
+          <div className={css.imageDiv} >
+            {/* <img src={snaks} >
+            </img> */}
           </div>
-          <div className={css.miniBody}>
-            <div className={css.head1}>
-              {/* <label>User Name</label> */}
-              <input type="text" placeholder="Enter User Name" />
+          <div className={css.loginpage}>
+            <div className={css.headingText}>
+              <label htmlFor="logIn">Login</label>
             </div>
-            <div className={css.head1}>
-              {/* <label>Password</label> */}
-              <input type="password" placeholder="Enter Password" />
+            <div className={css.miniBody}>
+              <div className={css.head1}>
+                {/* <label>User Name</label> */}
+                <input type="text" placeholder="Enter User Name" />
+              </div>
+              
+              <div className={css.head1}>
+                {/* <label>Password</label> */}
+                <input type="password" placeholder="Enter Password" />
+              </div>
+              <div style={{color:"#fff",fontSize:"12px",opacity:"0.5",marginLeft:"100px",marginTop:"-15px"}} >
+                {" "}
+                Forgot password?{" "}
+                 
+              </div>
+              <div className={css.button}>
+                <button
+                  onClick={() => {
+                    alert("Log In ");
+                  }}
+                >
+                  Log In
+                </button>
+                 
+              </div>
+              <div style={{color:"#fff",fontSize:"12px",opacity:"0.5"}} >
+                {" "}
+                Dont have an account?{" "}
+                <span>
+                  <a rel="stylesheet" href="/">
+                    Register here
+                  </a>
+                </span>
+              </div>
+
+
             </div>
-            <div>
-              {" "}
-              create account?{" "}
-              <span>
-                <a rel="stylesheet" href="/">
-                  dfdfd
-                </a>
-              </span>
-            </div>
-            <button
-              onClick={() => {
-                alert("Log In ");
-              }}
-            >
-              Log In
-            </button>
-            <button onClick={nav(-1)}>Back</button>
           </div>
         </div>
+
       </div>
     </div>
   );
