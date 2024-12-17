980718542f50400594c0e9e8c2f6b177947b436a
unknown <yaseen507at@gmail.com>
2024-12-17 09:01:41 +0530

    Login LogOut

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "../Login/Login.module.css";
 import {
   Link,
@@ -8,52 +8,79 @@ import {
   useLocation,
 } from "react-router-dom";
 import snaks from "../../assets/snaks_bg12.png"
-export default function Login() {
-  const nav = useNavigate();
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import { images } from "../../constants";
+import '../../container/Gallery/Gallery.css';
 
-  // console.log(arr.)
+export default function Login() {
+  const [Register, setRegister] = useState(false)
+  const scrollRef = React.useRef(null);
+  const nav = useNavigate();
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  console.log(Register)
   return (
     <div className={css.homeBody}>
       <div className={css.home}>
         <div className={css.upperBox} >
           <div className={css.imageDiv} >
-            {/* <img src={snaks} >
-            </img> */}
+
+
           </div>
           <div className={css.loginpage}>
             <div className={css.headingText}>
-              <label htmlFor="logIn">Login</label>
+              <label htmlFor="logIn"> { Register? "Sign up": "Login"}</label>
             </div>
             <div className={css.miniBody}>
               <div className={css.head1}>
                 {/* <label>User Name</label> */}
                 <input type="text" placeholder="Enter User Name" />
               </div>
-              
+
               <div className={css.head1}>
                 {/* <label>Password</label> */}
                 <input type="password" placeholder="Enter Password" />
               </div>
-              <div style={{color:"#fff",fontSize:"12px",opacity:"0.5",marginLeft:"100px",marginTop:"-15px"}} >
+              <div style={{ color: "#fff", fontSize: "12px", opacity: "0.5", marginLeft: "100px", marginTop: "-15px" }} >
                 {" "}
                 Forgot password?{" "}
-                 
+
               </div>
               <div className={css.button}>
+              {  Register ?
                 <button
                   onClick={() => {
-                    alert("Log In ");
+                    nav("/")
                   }}
                 >
                   Log In
+                </button> :
+                <button
+                  onClick={() => {
+                    nav("/")
+                  }}
+                >
+                  Sign up
                 </button>
-                 
+                }
+
               </div>
-              <div style={{color:"#fff",fontSize:"12px",opacity:"0.5"}} >
+              <div style={{ color: "#fff", fontSize: "12px", opacity: "0.5" }} >
                 {" "}
                 Dont have an account?{" "}
-                <span>
-                  <a rel="stylesheet" href="/">
+                <span
+                  onClick={() => { setRegister(true) }}
+                  className={css.RegisterLine}
+                >
+                  <a rel="stylesheet" >
                     Register here
                   </a>
                 </span>
